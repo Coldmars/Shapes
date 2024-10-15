4c589fa4953dcd5e4213c1ffb8c30d41152c1785
Ашур <ashurbek99@gmail.com>
2024-10-15 14:26:30 +0300

    Создана библиотека вычисления площади и проект тестов

---
+++ src/Shapes.AreaCalculator/Class1.cs
@@ -0,0 +1,7 @@
+﻿namespace Shapes.AreaCalculator
+{
+    public class Class1
+    {
+
+    }
+}
---
+++ tests/Shapes.AreaCalculator.Tests/UnitTest1.cs
@@ -0,0 +1,11 @@
+namespace Shapes.AreaCalculator.Tests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
